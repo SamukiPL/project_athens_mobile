b3436ebf1232aeb45f21e82d28cf64bbdf8fa0c9
Michał Kajzar <michal.kajzar@gmail.com>
2021-06-04 12:26:38 +0200

    Date Time extension update (#66)

--- lib/athens_core/ext/date_time_extension.dart
+++ lib/athens_core/ext/date_time_extension.dart
@@ -3,4 +3,17 @@ extension DateTimeExtension on DateTime {
   static DateTime defaultFrom() => DateTime.now().subtract(Duration(days: 31));
   static DateTime defaultTo() => DateTime.now();
 
+  bool get isToday {
+    final now = DateTime.now();
+    return now.day == this.day &&
+        now.month == this.month &&
+        now.year == this.year;
+  }
+
+  bool get isYesterday {
+    final yesterday = DateTime.now().subtract(Duration(days: 1));
+    return yesterday.day == this.day &&
+        yesterday.month == this.month &&
+        yesterday.year == this.year;
+  }
 }
