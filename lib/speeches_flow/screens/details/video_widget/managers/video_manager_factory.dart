53d58d7b91ad5b1f9ff44fb3cfda26eb178ad35f
Patryk Brejdak <p.brejdak@gmail.com>
2021-10-14 00:17:34 +0200

    fast implement fijkplayer

--- lib/speeches_flow/screens/details/video_widget/managers/video_manager_factory.dart
+++ lib/speeches_flow/screens/details/video_widget/managers/video_manager_factory.dart
@@ -7,11 +7,11 @@ import 'package:project_athens/speeches_flow/screens/details/video_widget/manage
 class VideoManagerFactory {
 
   static VideoManager create(speechModel, isNormalSpeech, wakelock) {
-    if (Platform.isIOS) {
+    // if (Platform.isIOS) {
       return VideoManagerVlc(speechModel, isNormalSpeech, wakelock);
-    } else {
-      return VideoManagerPlayer(speechModel, isNormalSpeech, wakelock);
-    }
+    // } else {
+    //   return VideoManagerPlayer(speechModel, isNormalSpeech, wakelock);
+    // }
   }
 
 }
--- lib/speeches_flow/screens/details/video_widget/managers/video_manager_vlc.dart
+++ lib/speeches_flow/screens/details/video_widget/managers/video_manager_vlc.dart
@@ -1,3 +1,4 @@
+import 'package:fijkplayer/fijkplayer.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 import 'package:project_athens/athens_core/models/speech_model.dart';
@@ -22,8 +23,23 @@ class VideoManagerVlc extends VideoManager {
 
   @override
   Widget buildVideoWidget() {
-    return VlcPlayer(controller: controller, aspectRatio: 16 / 9);
+    // return VlcPlayer(controller: controller, aspectRatio: 16 / 9);
+    final player = FijkPlayer();
+    player.setDataSource(
+      speechModel.videoUrl.replaceAll("&nolimit=1", "").substring(0, speechModel.videoUrl.indexOf('&name=')),
+        autoPlay: true
+    );
+    return Container(
+        width: 400,
+        height: 400,
+        child:
+      FijkView(
+        player: player,
+      )
+    );
+
   }
+
 
   @override
   void dispose() {
