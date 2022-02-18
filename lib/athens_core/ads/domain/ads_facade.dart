5387fabf91308f62f2b869d19c3a4ba217ab9357
Michal Kajzar <michal.kajzar@gmail.com>
2022-02-18 20:48:49 +0100

    Comment for very weird dart feature!

--- lib/athens_core/ads/domain/ads_facade.dart
+++ lib/athens_core/ads/domain/ads_facade.dart
@@ -10,6 +10,7 @@ mixin AdsFacade<T extends BaseParams> on ListFacade<T> {
   Stream<Result<List<BaseModel>>> getItems() {
     return super.getItems().map((event) {
       if (event.isSuccess()) {
+        //To whom it may concern, it's just a dart thing
         final mutableModelsList = event.toSuccess().value.map((e) => e as BaseModel).toList(growable: true);
         final indexesToAdd = new List<int>.generate((event.toSuccess().value.length / 10).floor(), (index) => 5 + (10 * index));
         indexesToAdd.forEach((index) {
